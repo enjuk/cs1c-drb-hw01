ce2ddda042d6c2e74c7a0547d808e83f21483891
Christopher Lewis <chris@ChristophersMBP.attlocal.net>
2023-09-11 11:47:54 -0700

    added header file
    this .h file is used to store all function prototypes

---
+++ hw01.h
@@ -0,0 +1,15 @@
+#include <sstream>
+
+#ifndef HW01_H
+#define HW01_H
+
+int sumDigits(int num);
+int tripleNumber(int num);
+int reverseDigits(int num);
+void sortArray(int numbers[], const int AR_SIZE);
+void printArray(const int numbers[], const int AR_SIZE);
+void saveArray(const int numbers[], const int AR_SIZE, const char* fileName);
+
+
+
+#endif
